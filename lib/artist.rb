039300ddc4cb615e6455d045109b0a94b7401650
john_wisneski <john.j.wisneski@gmail.com>
2020-12-13 20:03:54 -0500

    commit tree 705ca4fb2fe977023f424560ce600ade32d1c6fa

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,25 @@
+class Artist
+    @@all = []
+    attr_accessor :name 
+    def self.all
+        @@all
+    end
 
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def new_song(name,genre)
+        Song.new(name,self,genre)
+    end
+
+    def songs
+        Song.all.select {|song| song.artist == self}
+    end
+
+    def genres
+        self.songs.map {|song| song.genre}
+    end
+
+end
